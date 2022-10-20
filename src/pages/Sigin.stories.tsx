0fa9fa808538ba9736c8ab5238e6ddce02ca69c5
waltercjunior <96214751+waltercjunior@users.noreply.github.com>
2022-10-20 15:31:48 -0300

    Ajuste on import at Sigin.stories

--- src/pages/Sigin.stories.tsx
+++ src/pages/Sigin.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import { within, userEvent, waitFor } from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
 import { rest } from 'msw'
-import { SignIn } from "./SignIn";
+import { SignIn } from "./Signin";
 
 export default {
     title: 'Pages/Sign In',
